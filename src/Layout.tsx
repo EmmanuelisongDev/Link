906f1d48611b7c381aad5a9948bb3e94d63a3814
Emmanuel Isong <emmisongdev@gmail.com>
2024-02-09 17:18:39 +0100

    Added appwrite

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import { Toaster } from "@/components/ui/toaster"
 export default function Layout() {
   return (
     <main className='flex h-screen'>
         <Outlet/>
+        <Toaster/>
     </main>
   )
 }
